d7eae9ccec281e05be84347427cba6873f7217ac
sorakang <ckang@phx.yearup.org>
2021-09-27 22:33:54 -0700

    master

--- 0/type.js
+++ 0/type.js
@@ -7,7 +7,7 @@ function setupTypewriter(t) {
         tag = "",
         writingTag = false,
         tagOpen = false,
-        typeSpeed = 50,
+        typeSpeed = 30,
       tempTypeSpeed = 0;
 
     var type = function() {
